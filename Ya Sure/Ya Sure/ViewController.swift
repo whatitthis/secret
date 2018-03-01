e5225a860c76656e77ce63c6f7dbee8e9e9d2353
Mark Jin <renyej@starstechnologies.com>
2018-03-01 09:52:33 -0500

    inital commit

---
+++ Ya Sure/Ya Sure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ya Sure
+//
+//  Created by Mark Jin on 2018-03-01.
+//  Copyright © 2018 Mark Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
